a715fb5a29add2ffb76edb52b04a41159ffeb63d
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-04 17:38:20 -0500

    userlike api routed

--- src/app/likepage/userlikes/userlikes.component.ts
+++ src/app/likepage/userlikes/userlikes.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-userlikes',
   templateUrl: './userlikes.component.html',
   styleUrls: ['./userlikes.component.css']
 })
-export class UserlikesComponent {
+export class UserlikesComponent implements OnInit {
+  animal: any;
 
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void { 
+    this.getAnimals();
+  }
+
+  getAnimals(): void {
+    this.http
+      .get('http://localhost:8080/api/animal/')
+      .subscribe((response) => {
+        console.log(response);
+        this.animal = response;
+      });
+  }
 }
