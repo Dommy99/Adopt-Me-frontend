6dce9ce71ccbf80edaa3a09a3fccc8a32aad9b12
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-05 23:15:29 -0500

    img-carousel animation added

--- src/app/login/img-carousel/img-carousel.component.ts
+++ src/app/login/img-carousel/img-carousel.component.ts
@@ -1,39 +1,32 @@
-// img-carousel.component.ts
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+// with help from https://codepen.io/julessmiles/pen/yJqQKK
+import { Component} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Animal } from '../../../app/animal/animal.module';
-
-interface ApiResponse {
-  data: Animal[];
-  message: string;
-}
-
+import { Subject} from 'rxjs';
 @Component({
   selector: 'app-img-carousel',
   templateUrl: './img-carousel.component.html',
   styleUrls: ['./img-carousel.component.css']
 })
-export class ImgCarouselComponent implements OnInit {
+export class ImgCarouselComponent{
+  parentSubject: Subject<string> = new Subject();
   animals: Animal[] = [];
-  @Output() activeAnimal = new EventEmitter<Animal>();
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
     this.getAnimals();
   }
 
-  getAnimals(): void {
-    this.http
-      .get<ApiResponse>('http://localhost:8080/api/animal/')
-      .subscribe((response) => {
-        console.log(response);
+  getAnimals() {
+    this.http.get<{data: Animal[], message: string}>('http://localhost:8080/api/animal/')
+      .subscribe(response => {
         this.animals = response.data;
       });
   }
 
-  onSlideChange(slideEvent: any): void {
-    const currentSlideIndex = slideEvent.current; 
-    this.activeAnimal.emit(this.animals[currentSlideIndex]);
+
+  cardAnimation(value: string) {
+    this.parentSubject.next(value);
   }
 }
+
+
