927d8f44d1bef98335fbc00484164990a662c401
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-06 12:01:02 -0500

    validation added

--- src/app/login/login-register/login-register.component.ts
+++ src/app/login/login-register/login-register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-login-register',
@@ -11,7 +11,7 @@ import { Observable } from 'rxjs';
 export class LoginRegisterComponent {
   form: FormGroup;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private authService: AuthService) {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required])
@@ -30,9 +30,13 @@ export class LoginRegisterComponent {
   login(): void {
     if (this.form.valid) {
       this.http.post('http://localhost:8080/auth/users/login/', this.form.value).subscribe(
-        (response) => console.log(response),
+        (response: any) => {
+          console.log(response);
+          this.authService.login(response.userId);
+        },
         (error) => console.log(error)
       );
     }
   }
+  
 }
