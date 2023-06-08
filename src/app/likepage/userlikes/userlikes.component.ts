ea88fb0638b4409ed96e091ceb4f096600467c4e
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-07 21:12:12 -0500

    updated get request

--- src/app/likepage/userlikes/userlikes.component.ts
+++ src/app/likepage/userlikes/userlikes.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../../app/auth.service';
 import { Animal } from '../../../app/animal/animal.module';
-
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-userlikes',
@@ -36,20 +36,60 @@ export class UserlikesComponent implements OnInit, OnDestroy {
 
   getAnimals(): void {
     if (this.userId) {
+      let headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.authService.getToken()}`
+    });
+    
       this.http
-        .get<{data: Animal[], message: string}>(`http://localhost:8080/api/user/${this.userId}/likes`)
-        .subscribe((response) => {
-          console.log(response);
-          this.animals = response.data;
-          console.log('Animals: ', this.animals);
-        });
+        .get<{data: Animal[], message: string}>(`http://localhost:8080/api/user/${this.userId}/likes`, { headers: headers })
+        .subscribe(
+          (response) => {
+            console.log(response);
+            this.animals = response.data;
+            console.log('Animals: ', this.animals);
+          },
+          (error) => {
+            console.error('Error:', error);
+          }
+        );
+        
     }
   }
+
+//   async getAnimals(): Promise<void> {
+//     if (this.userId) {
+//         let headers = new HttpHeaders({
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer ${this.authService.getToken()}`
+//         });
+//         try {
+//             const response = await this.http
+//             .get<{data: Animal[], message: string}>(`http://localhost:8080/api/user/${this.userId}/likes`, { headers: headers })
+//             .toPromise();
+//             if (response) {
+//                 console.log(response);
+//                 this.animals = response.data;
+//                 console.log('Animals: ', this.animals);
+//             } else {
+//                 console.error('No response from the server');
+//             }
+//         } catch(error) {
+//             console.error('Error:', error);
+//         }
+//     }
+// }
+
 
   removeAnimal(id: number): void {
     this.http.delete(`http://localhost:8080/api/like/${id}`).subscribe(response => {
       this.getAnimals();
     });
   }
+
+  onAnimalLiked(animal: Animal) {
+    this.animals.push(animal);
+  }
+  
 }
 
