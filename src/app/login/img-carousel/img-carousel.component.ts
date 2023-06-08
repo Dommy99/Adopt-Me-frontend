96748de80871669a9ab53745429fe2cf36b7b205
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-07 19:58:59 -0500

    added auth to cardanimation1

--- src/app/login/img-carousel/img-carousel.component.ts
+++ src/app/login/img-carousel/img-carousel.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Animal } from '../../../app/animal/animal.module';
 import { Subject } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../../../app/auth.service';
+
 @Component({
   selector: 'app-img-carousel',
   templateUrl: './img-carousel.component.html',
@@ -11,11 +14,15 @@ export class ImgCarouselComponent {
   parentSubject: Subject<string> = new Subject();
   animals: Animal[] = [];
   likedAnimals: Animal[] = [];
+  currentAnimal: Animal | null = null;
+
+
   
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private authService: AuthService) {
     this.getAnimals();
   }
+  
 
   getAnimals() {
     this.http.get<{data: Animal[], message: string}>('http://localhost:8080/api/animal/')
@@ -23,15 +30,31 @@ export class ImgCarouselComponent {
         this.animals = response.data;
       });
   }
+  // private headers = new HttpHeaders().set('Authorization', 'Bearer ' +'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsImlhdCI6MTY4NjE3NTgwMCwiZXhwIjoxNjg2MjYyMjAwfQ.66bMCM5UlpNMzrqxcIeBuLaetelGRkh3yMpdqk2tJmE' );
 
-  cardAnimation1(value: string,animal: Animal) {
+  cardAnimation1(value: string) {
     this.parentSubject.next(value);
-    this.http.post(`http://localhost:8080/api/like/${animal.id}`, {}).subscribe(response => {
-      this.likedAnimals.push(animal);
-      console.log(response);
-      
-    });
+    if (this.currentAnimal) {
+      const headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.authService.currentToken);
+      this.http.post(`http://localhost:8080/api/like/${this.currentAnimal.id}`, {}, { headers: headers })
+        .subscribe(response => {
+          this.likedAnimals.push(this.currentAnimal!);
+          console.log(response);
+        });
+    }
   }
+  
+
+  // cardAnimation1(value: string) {
+  //   this.parentSubject.next(value);
+  //   if (this.currentAnimal) {
+  //     this.http.post(`http://localhost:8080/api/like/${this.currentAnimal.id}`, {}).subscribe(response => {
+  //       this.likedAnimals.push(this.currentAnimal!);
+  //       console.log(response);
+  //     });
+  //   }
+  // }
+  
   cardAnimation2(value: string) {
     this.parentSubject.next(value);
   }
