8069b531f0226c6fa090870e57317910375665bc
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-07 20:00:24 -0500

    error handling and auth added

--- src/app/login/login-register/login-register.component.ts
+++ src/app/login/login-register/login-register.component.ts
@@ -12,7 +12,11 @@ import { AuthService } from 'src/app/auth.service';
 export class LoginRegisterComponent {
   form: FormGroup;
 
-  constructor(private http: HttpClient, private authService: AuthService, private router: Router) {
+  constructor(
+    private http: HttpClient, 
+    private authService: AuthService, 
+    private router: Router
+  ) {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required])
@@ -21,23 +25,40 @@ export class LoginRegisterComponent {
 
   register(): void {
     if (this.form.valid) {
+      console.log(this.form.value);
+      
       this.http.post('http://localhost:8080/auth/users/register/', this.form.value).subscribe(
-        (response) => console.log(response),
-        (error) => console.log(error)
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+          if (error.status === 409) {
+            alert('This user already exists. Please try a different email.');
+          } else {
+            alert('An error occurred. Please try again later.');
+          }
+        }
       );
     }
   }
+  
 
   login(): void {
-    if (this.form.valid) {
-      this.http.post('http://localhost:8080/auth/users/login/', this.form.value).subscribe(
-        (response: any) => {
-          console.log(response);
-          this.authService.login(response.userId);
-          this.router.navigate(['/login-img-carousel']);  // navigate after login
-        },
-        (error) => console.log(error)
-      );
-    }
+  if (this.form.valid) {
+    this.http.post('http://localhost:8080/auth/users/login/', this.form.value).subscribe(
+      (response: any) => {
+        console.log("Response from server: ", response);
+        if (response && response.data && response.userId) { 
+          this.authService.login(response.data, response.userId); 
+          this.router.navigate(['/login-img-carousel']); 
+        }
+      },
+      (error) => console.log(error)
+    );
   }
+}
+
+  
+  
 }
